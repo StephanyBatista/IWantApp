bf98cdc95bada7b1de27b97e20f483c56a1918a5
Stephany Batista <stephany.batista@nibo.com.br>
2022-01-09 14:55:41 -0400

    Fix error syntax

--- src/Endpoints/Categories/CategoryGetAll.cs
+++ src/Endpoints/Categories/CategoryGetAll.cs
@@ -10,7 +10,7 @@ public class CategoryGetAll
     public static IResult Action(ApplicationDbContext context)
     {
         var categories = context.Categories.ToList();
-        var response = categories.Select(c => new CategoryResponse { Id = c.Id, Name = c.Name, Active = c.Active });
+        var response = categories.Select(c => new CategoryResponse(c.Id, c.Name, c.Active));
         
         return Results.Ok(response);
     }
