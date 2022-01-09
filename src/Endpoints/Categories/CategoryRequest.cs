4e0d2c1c2259d811982e9bee7b28b0ae5daa533c
Stephany Batista <stephany.batista@nibo.com.br>
2022-01-09 12:58:34 -0400

    Converting DTO class to record

--- src/Endpoints/Categories/CategoryRequest.cs
+++ src/Endpoints/Categories/CategoryRequest.cs
@@ -1,7 +1,3 @@
 ﻿namespace IWantApp.Endpoints.Categories;
 
-public class CategoryRequest
-{
-    public string Name { get; set; }
-    public bool Active { get; set; }
-}
+public record CategoryRequest(string Name, bool Active);
--- src/Endpoints/Categories/CategoryResponse.cs
+++ src/Endpoints/Categories/CategoryResponse.cs
@@ -1,8 +1,3 @@
 ﻿namespace IWantApp.Endpoints.Categories;
 
-public class CategoryResponse
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public bool Active { get; set; }
-}
+public record CategoryResponse(Guid Id, string Name, bool Active);
