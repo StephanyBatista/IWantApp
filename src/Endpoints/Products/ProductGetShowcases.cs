1720c4ca2ad053a21041276d07318f38866499f2
Stephany Batista <stephany.batista@nibo.com.br>
2022-01-22 11:41:16 -0400

    Validating data on /products/showcase

--- src/Endpoints/Products/ProductGetShowcases.cs
+++ src/Endpoints/Products/ProductGetShowcases.cs
@@ -7,24 +7,22 @@ public class ProductGetShowcase
     public static Delegate Handle => Action;
 
     [AllowAnonymous]
-    public static async Task<IResult> Action(int? page, int? row, string? orderBy, ApplicationDbContext context)
+    public static async Task<IResult> Action(ApplicationDbContext context, int page = 1, int row = 10, string orderBy = "name")
     {
-        if(page == null)
-            page = 1;
-        if(row == null)
-            row = 10;
-        if(string.IsNullOrEmpty(orderBy))
-            orderBy = "name";
-
+        if (row > 10)
+            return Results.Problem(title: "Row with max 10", statusCode: 400);
+        
         var queryBase = context.Products.Include(p => p.Category)
             .Where(p => p.HasStock && p.Category.Active);
 
         if (orderBy == "name")
             queryBase = queryBase.OrderBy(p => p.Name);
-        else
+        else if (orderBy == "price")
             queryBase = queryBase.OrderBy(p => p.Price);
+        else
+            return Results.Problem(title: "Order only by price or name", statusCode: 400);
 
-        var queryFilter = queryBase.Skip((page.Value - 1) * row.Value).Take(row.Value);
+        var queryFilter = queryBase.Skip((page - 1) * row).Take(row);
 
         var products = queryFilter.ToList();
 
