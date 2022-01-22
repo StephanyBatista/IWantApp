22d28055dff442da6d3160e6ae9a574c69f19a87
Stephany Batista <stephany.batista@nibo.com.br>
2022-01-22 11:31:36 -0400

    Paginate on /products/showcase

--- src/Endpoints/Products/ProductGetShowcases.cs
+++ src/Endpoints/Products/ProductGetShowcases.cs
@@ -7,11 +7,27 @@ public class ProductGetShowcase
     public static Delegate Handle => Action;
 
     [AllowAnonymous]
-    public static async Task<IResult> Action(ApplicationDbContext context)
+    public static async Task<IResult> Action(int? page, int? row, string? orderBy, ApplicationDbContext context)
     {
-        var products = context.Products.Include(p => p.Category)
-            .Where(p => p.HasStock && p.Category.Active)
-            .OrderBy(p => p.Name).ToList();
+        if(page == null)
+            page = 1;
+        if(row == null)
+            row = 10;
+        if(string.IsNullOrEmpty(orderBy))
+            orderBy = "name";
+
+        var queryBase = context.Products.Include(p => p.Category)
+            .Where(p => p.HasStock && p.Category.Active);
+
+        if (orderBy == "name")
+            queryBase = queryBase.OrderBy(p => p.Name);
+        else
+            queryBase = queryBase.OrderBy(p => p.Price);
+
+        var queryFilter = queryBase.Skip((page.Value - 1) * row.Value).Take(row.Value);
+
+        var products = queryFilter.ToList();
+
         var results = products.Select(p => new ProductResponse(p.Name, p.Category.Name, p.Description, p.HasStock, p.Price, p.Active));
         return Results.Ok(results);
     }
